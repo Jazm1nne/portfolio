549646f6e2e346b8cfb6847b971854ed0386e7fd
Gaspare Tortora <g.tortora2@gmail.com>
2023-07-05 09:38:28 -0400

    Added form to main page

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import { appearAboutImage } from "./about.js"
 import { typeWriter } from "./typing.js";
+import handleForm from "./form.js";
 
 function init() {
     appearAboutImage();
     typeWriter(0);
+    handleForm();
 }
 init()
   
