45ac99222cca4fcccaf8b58642ac3322327f2edc
Gaspare Tortora <g.tortora2@gmail.com>
2023-07-10 11:19:46 -0400

    Fixed issue with class removal

--- project.js
+++ project.js
@@ -4,10 +4,12 @@ const showProject = () => {
         button.addEventListener('click', () => {
             const parentElement = button.parentElement;
             if (button.textContent === 'Show project') {
+                parentElement.classList.remove('close-project')
                 parentElement.classList.add('open-project')
                 parentElement.querySelector('iframe').style.opacity = "1";
                 button.textContent = 'Show less'
             } else {
+                parentElement.classList.remove('open-project')
                 parentElement.classList.add('close-project')
                 button.textContent = 'Show project'
                 parentElement.querySelector('iframe').style.opacity = "0";
