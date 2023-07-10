483ad8cf120fb2d40e06b109aaa903161f9635dc
Gaspare Tortora <g.tortora2@gmail.com>
2023-07-09 20:44:39 -0400

    Created project appearance functionality

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ import { appearAboutImage } from "./about.js"
 import { typeWriter } from "./typing.js";
 import handleForm from "./form.js";
 import showProject from "./project.js";
+import projectEffect from "./projectEffect.js";
 
 function init() {
     appearAboutImage();
     typeWriter(0);
     handleForm();
     showProject();
+    projectEffect();
 }
+
 init()
   
