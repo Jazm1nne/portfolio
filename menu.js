44c0806f95c63231438191f73e6d5d1a6da29ff1
Gaspare Tortora <g.tortora2@gmail.com>
2023-07-18 07:17:14 -0400

    Created mobile menu functionality

---
+++ menu.js
@@ -0,0 +1,19 @@
+const openMenu = () => {
+    const burgerIcon = document.querySelector('.hamburger-menu');
+    const menuList = document.querySelector('.mobile-list');
+    const menuContainer = document.querySelector('.mobile-header-menu');
+    
+    burgerIcon.addEventListener('click', () => {
+        menuContainer.classList.toggle('open-menu');
+        menuList.classList.toggle('show-menu');
+        burgerIcon.classList.toggle('is-active');
+    })
+
+    menuList.addEventListener('click', () => {
+        menuContainer.classList.toggle('open-menu');
+        menuList.classList.toggle('show-menu');
+        burgerIcon.classList.toggle('is-active');
+    })
+};
+
+export default openMenu;
--- theme.js
+++ theme.js
@@ -1,25 +1,30 @@
 const changeTheme = () => {
     const theme = document.getElementById('theme');
-    const button = document.querySelector('.theme-button');
-    const icon = document.querySelector('.theme-icon')
-    console.log(theme.attributes.href.textContent)
+    const buttons = document.querySelectorAll('.theme-button');
+    const icons = document.querySelectorAll('.theme-icon')
 
-    button.addEventListener('click', () => {
-        if (button.textContent === "Light") {
-                button.textContent = "Dark";
-                icon.classList.remove('fa-sun');
-                icon.classList.add('fa-moon');
-                theme.attributes.href.value = "./styles/light.css"
-        }   else {
-                button.textContent = "Light";
-                icon.classList.remove('fa-moon');
-                icon.classList.add('fa-sun');
-                theme.attributes.href.value = "./styles/dark.css"
-                console.log(theme.attributes.href.textContent)
-        } 
-
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            if (button.textContent === "Light") {
+                    button.textContent = "Dark";
+                    icons.forEach((icon) => {
+                        icon.classList.remove('fa-sun');
+                        icon.classList.add('fa-moon');
+                    })
+                    theme.attributes.href.value = "./styles/light.css"
+            }   else {
+                    button.textContent = "Light";
+                    icons.forEach((icon) => {
+                        icon.classList.remove('fa-moon');
+                        icon.classList.add('fa-sun');   
+                    })
+                    theme.attributes.href.value = "./styles/dark.css"
+                    console.log(theme.attributes.href.textContent)
+            } 
+    
+        })
     })
-    console.log()
+
 }
 
 export default changeTheme;
